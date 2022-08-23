42665ee44c03684aca52a1a6a72367010c4b4485
kdo2006 <106128975+kdo2006@users.noreply.github.com>
2022-08-23 18:14:56 +0545

    Add files via upload

---
+++ spam.py
@@ -0,0 +1,27 @@
+import pywhatkit
+import pyfiglet
+
+
+ascii_banner = pyfiglet.figlet_format("Whatsapp Spammer by K.D.O")
+print(ascii_banner)
+
+# Asks a user to type a phonenumber to message
+
+
+print("\n")
+print("Enter Target Number with Country code:",end=""),
+number=(input())
+
+# Asks for user to type a message
+print("\n")
+print("Enter message:",end=""),
+message=str(input())
+
+
+print("Enter massage amount:",end=""),
+tim = int(input())
+
+for i in range(tim):
+    pywhatkit.sendwhatmsg_instantly(number, message, 10)
+# Send a WhatsApp Message to the contact instantly (gives 10s to load web client before sending)
+
